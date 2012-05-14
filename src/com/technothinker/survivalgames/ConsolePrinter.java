48012aceaf91d652a69670e1daad9f93fe455145
Bannu <bannu1@comcast.net>
2012-05-14 17:39:16 -0400

    edit

--- src/com/technothinker/survivalgames/ConsolePrinter.java
+++ src/com/technothinker/survivalgames/ConsolePrinter.java
@@ -3,24 +3,28 @@ package com.technothinker.survivalgames;
 import java.util.logging.Logger;
 
 import org.bukkit.GameMode;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Class1 extends JavaPlugin {
+public class ConsolePrinter extends JavaPlugin {
+	
 	public static SurvivalGames plugin;
+	
 	public final Logger logger = Logger.getLogger("Minecraft");
 	
 	@Override
 	public void onDisable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
-	}	
 	
+	}	
 	@Override
 	public void onEnable() {
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.logger.info(pdfFile.getName() + " Has Been Enabled!");
+		
 	}
-	
 
 }
