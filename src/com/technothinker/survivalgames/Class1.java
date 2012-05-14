799b88a28573aba1f056f18a3cf4103669fa1264
Bannu <bannu1@comcast.net>
2012-05-13 20:07:26 -0400

    Test

--- src/com/technothinker/survivalgames/Class1.java
+++ src/com/technothinker/survivalgames/Class1.java
@@ -1,7 +1,11 @@
 package com.technothinker.survivalgames;
 
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Class1 extends JavaPlugin{
+public class Class1 extends JavaPlugin {
+	public static SurvivalGames plugin;
+	public final Logger logger = logger.getLogger("Minecraft);
 
 }
