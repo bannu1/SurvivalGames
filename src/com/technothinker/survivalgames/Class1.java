2a79292e0bc9fa626e7cf8bc1ff76eb17d112d26
Bannu <bannu1@comcast.net>
2012-05-14 17:21:19 -0400

    Add

--- src/com/technothinker/survivalgames/Class1.java
+++ src/com/technothinker/survivalgames/Class1.java
@@ -2,10 +2,24 @@ package com.technothinker.survivalgames;
 
 import java.util.logging.Logger;
 
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Class1 extends JavaPlugin {
 	public static SurvivalGames plugin;
-	public final Logger logger = logger.getLogger("Minecraft);
+	public final Logger logger = Logger.getLogger("Minecraft");
+	
+	@Override
+	public void onDisable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Has Been Disabled!");
+	}	
+	
+	@Override
+	public void onEnable() {
+		PluginDescriptionFile pdfFile = this.getDescription();
+		this.logger.info(pdfFile.getName() + " Has Been Enabled!");
+	}
+	
 
 }
